4480668fcd77157c8ddbb0affd10bd4a4aa64d7a
Ramprasath.G <ramprasath.ganesan@gmail.com>
2026-03-18 16:42:02 +0530

    fix: empty Users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,8 @@
 import { NavLink } from "react-router-dom";
-
+import { useUser } from "../context/useUser";
 function Users() {
  
-    const users=localStorage.getItem("Users")||[];
+    const {users} = useUser();
 
  
   return (
